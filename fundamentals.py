8a0803c5ad3c55465e185ffa22118d20ab47b842
nickguerra <nickg@maliciousgames.com>
2023-04-08 06:17:30 -0500

    work with autograd

--- fundamentals.py
+++ fundamentals.py
@@ -1,4 +1,13 @@
-import torch
+# Virtual environment @ d:\Downloads\Anaconda
+# current GPU does not support CUDA (GeForce RTX 2070 super [only 7.5 compute from wiki])
+# open anaconda prompt
+# activate pytorch to activate
 
-x = torch.empty(2)
+import torch
+import numpy as np
+
+print(torch.__version__)
+print(torch.cuda.is_available())
+
+x = torch.ones(5, requires_grad=True)
 print(x)
